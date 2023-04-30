6c0284ed276ec7257f185f73c5e6742c1f39700d
prosper <edwardprosper001@gmail.com>
2023-04-30 06:10:38 +0100

    'updates'

--- learn_ed_api/settings.py
+++ learn_ed_api/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['learn-ed-app.herokuapp.com']
 
 
 # Application definition
